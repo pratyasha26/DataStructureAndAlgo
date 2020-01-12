fda01e48a8e53bbef8cb48f2165db2058d9383a0
Pratyasha Pratik <pratyasha.pratik@cleartrip.com>
2020-01-12 14:47:53 +0530

    backtrackin implemented

--- src/com/company/Nqueen.java
+++ src/com/company/Nqueen.java
@@ -3,43 +3,39 @@ package com.company;
 public class Nqueen {
     static int[][] ar;
     int n;
-    int flag;
 
     public Nqueen(int n) {
         this.n = n;
         ar = new int[n][n];
     }
 
-    public  boolean checkEachColumnForQueen(int col, int n, int[][] ar) {
-        for (int row = 0; row < n; row++) {
-            if (checkForQueenHorizontally(col, row, n) && checkForQueenVertically(col, row, n) && checkForQueenDiagonally(col,row,n)) {
-                ar[row][col] = 1;
-                return true;
-            }
+    public static boolean checkEachColumnForQueen(int col, int row, int n, int[][] ar) {
+
+        if (checkForQueenHorizontally(col, row, n) && checkForQueenVertically(col, row, n) && checkForQueenDiagonally(col, row, n)) {
+            ar[row][col] = 1;
+            return true;
         }
+
         return false;
     }
 
-    private boolean checkForQueenDiagonally(int col, int row, int n) {
-    if(checkForQueenInRightDiagonal(col,row,n) && checkForQueenInLeftDiagonal(col,row,n)){
-        return true;
-    }
-    return false;
+    private static boolean checkForQueenDiagonally(int col, int row, int n) {
+        return checkForQueenInRightDiagonal(col, row, n) && checkForQueenInLeftDiagonal(col, row, n);
     }
 
     private static boolean checkForQueenInLeftDiagonal(int col, int row, int n) {
-        int left=row-1;
-        int up=col-1;
-        int down=col+1;
-        while(left>=0 && up>=0){
-            if(ar[left][up]==1){
+        int left = row - 1;
+        int up = col - 1;
+        int down = col + 1;
+        while (left >= 0 && up >= 0) {
+            if (ar[left][up] == 1) {
                 return false;
             }
             left--;
             up--;
         }
-        while(left>=0 && down<n){
-            if(ar[left][down]==1){
+        while (left >= 0 && down < n) {
+            if (ar[left][down] == 1) {
                 return false;
             }
             left--;
@@ -49,18 +45,18 @@ public class Nqueen {
     }
 
     private static boolean checkForQueenInRightDiagonal(int col, int row, int n) {
-        int right=row+1;
-        int up=col-1;
-        int down=col+1;
-        while(right<n && up>=0){
-            if(ar[right][up]==1){
+        int right = row + 1;
+        int up = col - 1;
+        int down = col + 1;
+        while (right < n && up >= 0) {
+            if (ar[right][up] == 1) {
                 return false;
             }
             right++;
             up--;
         }
-        while(right<=n && down<n){
-            if(ar[row][col]==1){
+        while (right <= n && down < n) {
+            if (ar[row][col] == 1) {
                 return false;
             }
             right++;
@@ -97,7 +93,7 @@ public class Nqueen {
     }
 
     private static boolean checkForQueenLeft(int row, int col, int n) {
-        for (int left = row-1; left >=0; left--) {
+        for (int left = row - 1; left >= 0; left--) {
             if (ar[left][col] == 1) {
                 return false;
             }
@@ -106,7 +102,7 @@ public class Nqueen {
     }
 
     private static boolean checkForQueenRight(int row, int col, int n) {
-        for (int right = row+1; right <n; right++) {
+        for (int right = row + 1; right < n; right++) {
             if (ar[right][col] == 1) {
                 return false;
             }
@@ -119,19 +115,33 @@ public class Nqueen {
         int n = 4;
         Nqueen nqueen = new Nqueen(n);
 
-        for (int col = 0; col < n; col++) {
-            boolean isQueenPlaced = nqueen.checkEachColumnForQueen(col, n, ar);
-            if(isQueenPlaced){
-
-            }
+        int col = 0;
+        if (!solveNqUtil(col, n, ar)) {
+            System.out.println("Solution does not exist");
         }
-        printTheChessBoard(ar,n);
+
+        printTheChessBoard(ar, n);
 
     }
 
+    private static boolean solveNqUtil(int col, int n, int[][] ar) {
+        if (col >= n)
+            return true;
+        for (int row = 0; row < n; row++) {
+            boolean isQueenPlaced = checkEachColumnForQueen(col, row, n, ar);
+            if (isQueenPlaced) {
+                if (solveNqUtil(col + 1, n, ar)) {
+                    return true;
+                }
+                ar[row][col] = 0;
+            }
+        }
+        return false;
+    }
+
     private static void printTheChessBoard(int[][] ar, int n) {
-        for(int row=0;row<n;row++){
-            for(int col=0;col<n;col++){
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
                 System.out.print(ar[row][col]);
             }
             System.out.println();
