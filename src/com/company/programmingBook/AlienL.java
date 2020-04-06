33819608c905baab7a4672d1aacfe206474ac7a5
Pratyasha Pratik <pratyasha.pratik@cleartrip.com>
2020-04-06 23:28:53 +0530

    for all patterns

--- src/com/company/programmingBook/AlienL.java
+++ src/com/company/programmingBook/AlienL.java
@@ -3,28 +3,48 @@ package com.company.programmingBook;
 public class AlienL {
 
     public static void  main(String args[]) {
+
+        String pattern[] = {"(ab) (bc) (ca)","abc",
+                "(abc) (abc) (abc)",
+                "(zyx) bc"};
+        for(int p=0;p<pattern.length;p++) {
+            System.out.println("pattern : " +(p+1));
+            getAlienCount(pattern[p]);
+        }
+    }
+
+    private static void getAlienCount( String pattern) {
         String words[] = {"abc",
                 "bca",
                 "dac",
                 "dbc",
                 "cba"};
-        String pattern = "(ab) (bc) (ca)";
+        int count = 0;
         for (int j = 0; j < words.length; j++) {
             char[] wordCharArray = words[j].toCharArray();
-            String[] sPattern = pattern.split(" ");
-            int count = 0;
-            for (int i = 0; i < words[j].length(); i++) {
-                char c = wordCharArray[i];
-                int index = sPattern[i].indexOf(c);
-                if (index != -1) {
-                    count++;
-                    continue;
-                } else {
-                    count = 0;
-                    break;
+            if(!pattern.contains(" ")) {
+                if(words[j].equals(pattern)){
+                    count= pattern.length();
+                    System.out.println(words[j]+ ": "+count);
                 }
             }
-            System.out.println(words[j]+ ": "+count);
+            else {
+                String[] sPattern = pattern.split(" ");
+
+                for (int i = 0; i < words[j].length(); i++) {
+                    char c = wordCharArray[i];
+                    int index = sPattern[i].indexOf(c);
+                    if (index != -1) {
+                        count++;
+                        continue;
+                    } else {
+                        count = 0;
+                        break;
+                    }
+                }
+                System.out.println(words[j]+ ": "+count);
+            }
+           count=0;
         }
     }
 }
